dfe1b2f8246dd5f089edb28cfb871e9b357e86e0
Jacob Lifshay <programmerjake@gmail.com>
2014-03-17 12:55:37 -0700

    fixed physics

--- src/entity_block.cpp
+++ src/entity_block.cpp
@@ -95,7 +95,7 @@ void EntityBlock::onMove(EntityData & data, shared_ptr<World> world, float delta
                             case PhysicsObject::Type::None:
                                 break;
                             }
-                            if(filled && false)
+                            if(filled)
                             {
                                 firstCollision.time = 0;
                                 firstCollision.newPosition = data.position;
